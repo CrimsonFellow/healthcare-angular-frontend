9ec166a84538844cc6b5f9bed659e5c15278b8ba
CrimsonFellow <eldrigedongallego@gmail.com>
2024-09-17 10:52:02 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [RouterOutlet,CommonModule],
+  templateUrl:'./app.component.html' ,
+  styleUrls: ['./app.component.css'] ,
 })
 export class AppComponent {
   title = 'healthcare-app';
+  showAdminHeading = false;  
+
+  constructor(private router: Router) {
+    
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+       
+        this.showAdminHeading = this.router.url.startsWith('/admin');
+      }
+    });
+  }
 }
+
+
